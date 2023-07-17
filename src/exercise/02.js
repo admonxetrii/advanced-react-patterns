789447a1586c33aae9513286768d58df7c1bbd29
Nisham Wagle <nishamwagle@gmail.com>
2023-07-17 16:46:20 +0545

    finished advanced react patterns

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,40 +4,32 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
-
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://react.dev/reference/react/Children
-  // 📜 https://react.dev/reference/react/cloneElement
-  return <Switch on={on} onClick={toggle} />
+function Toggle({children}) {
+    const [on, setOn] = React.useState(false)
+    const toggle = () => setOn(!on)
+    return React.Children.map(children, child => {
+        if (typeof child.type === 'string') {
+            return child
+        }
+        return React.cloneElement(child, {on, toggle})
+    })
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleOn = ({on, children}) => on ? children : null
+const ToggleOff = ({on, children}) => on ? null : children
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle}/>
 
 function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
-      </Toggle>
-    </div>
-  )
+    return (
+        <div>
+            <Toggle>
+                <ToggleOn>The button is on</ToggleOn>
+                <ToggleOff>The button is off</ToggleOff>
+                <span>Hello</span>
+                <ToggleButton />
+            </Toggle>
+        </div>
+    )
 }
 
 export default App
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,27 +4,49 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function useToggle() {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
+function callAll(...fns) {
+    return (...args) => {
+        fns.forEach(fn => {
+            fn && fn(...args)
+        })
+    }
+}
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+function useToggle() {
+    const [on, setOn] = React.useState(false)
+    const toggle = () => setOn(!on)
+
+    function getTogglerProps({onClick, ...props}) {
+        return {
+            'aria-pressed': on,
+            onClick: callAll(onClick, toggle),
+            ...props,
+        }
+    }
+
+    return {
+        on, toggle,
+        getTogglerProps,
+    }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
-  return (
-    <div>
-      <Switch on={on} {...togglerProps} />
-      <hr />
-      <button aria-label="custom-button" {...togglerProps}>
-        {on ? 'on' : 'off'}
-      </button>
-    </div>
-  )
+    const {on, getTogglerProps} = useToggle()
+    return (
+        <div>
+            <Switch {...getTogglerProps({on})} />
+            <hr/>
+            <button
+                aria-label="custom-button"
+                {...getTogglerProps({
+                    'aria-label': 'custom-button',
+                    onClick: () => console.log('awesome'),
+                })}
+            >
+                {on ? 'on' : 'off'}
+            </button>
+        </div>
+    )
 }
 
 export default App
